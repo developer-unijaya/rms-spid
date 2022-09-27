8eb4dd2f9b3fdcaf867714f9e07bda76e66ab8c6
Syafiq Unijaya <syafiqazwan.unijaya@gmail.com>
2022-09-27 15:27:50 +0800

    Update PSR-4

--- src/RMSSpid.php
+++ src/RMSSpid.php
@@ -2,14 +2,12 @@
 
 namespace DeveloperUnijaya\RmsSpid;
 
-use Illuminate\Support\Facades\Http;
+// use Illuminate\Support\Facades\Http;
 
 class RmsSpid
 {
     public function justDoIt()
     {
-        $response = Http::get('https://inspiration.goprogram.ai/');
-
-        return $response['quote'] . ' -' . $response['author'];
+        return "Hok aloh";
     }
 }
