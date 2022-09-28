c436ce2c197c1d685610c0749427038bed17705e
Syafiq Unijaya <syafiqazwan.unijaya@gmail.com>
2022-09-28 10:47:34 +0800

    0.1.11

--- src/routes/api.php
+++ src/routes/api.php
@@ -2,28 +2,30 @@
 
 use DeveloperUnijaya\RmsSpid\Controllers\RmsSpidAuthController;
 use DeveloperUnijaya\RmsSpid\Controllers\RmsSpidUserController;
-use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-Route::prefix('spid')->group(function () {
+Route::prefix('api')->group(function () {
 
-    Route::prefix('auth')->group(function () {
+    Route::prefix('spid')->group(function () {
 
-        Route::post('login', [RmsSpidAuthController::class, 'login'])->name('spid.auth.login');
+        Route::prefix('auth')->group(function () {
 
-        Route::middleware('auth:sanctum')->group(function () {
-            Route::post('me', [RmsSpidAuthController::class, 'me'])->middleware('auth:sanctum')->name('spid.auth.me');
-            Route::post('logout', [RmsSpidAuthController::class, 'logout'])->middleware('auth:sanctum')->name('spid.auth.logout');
+            Route::post('login', [RmsSpidAuthController::class, 'login'])->name('spid.auth.login');
+
+            Route::middleware('auth:sanctum')->group(function () {
+                Route::post('me', [RmsSpidAuthController::class, 'me'])->middleware('auth:sanctum')->name('spid.auth.me');
+                Route::post('logout', [RmsSpidAuthController::class, 'logout'])->middleware('auth:sanctum')->name('spid.auth.logout');
+            });
+
+        });
+
+        Route::prefix('user')->middleware('auth:sanctum')->group(function () {
+            Route::post('register', [RmsSpidUserController::class, 'register'])->name('spid.user.register');
+            Route::post('profile', [RmsSpidUserController::class, 'profile'])->name('spid.user.profile');
+
+            // To be used by Sub-system
+            Route::post('redirect', [RmsSpidUserController::class, 'redirect'])->name('spid.user.redirect');
         });
 
     });
-
-    Route::prefix('user')->middleware('auth:sanctum')->group(function () {
-        Route::post('register', [RmsSpidUserController::class, 'register'])->name('spid.user.register');
-        Route::post('profile', [RmsSpidUserController::class, 'profile'])->name('spid.user.profile');
-
-        // To be used by Sub-system
-        Route::post('redirect', [RmsSpidUserController::class, 'redirect'])->name('spid.user.redirect');
-    });
-
 });
