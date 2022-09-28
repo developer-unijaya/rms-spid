309770551156abee2ec4c4fd73326d38077c9f7d
Syafiq Unijaya <syafiqazwan.unijaya@gmail.com>
2022-09-28 16:51:39 +0800

    0.1.24

--- src/routes/web.php
+++ src/routes/web.php
@@ -5,7 +5,7 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('spid-test', [RmsSpidController::class, 'testSpid'])->name('spid.test');
 
-Route::prefix('spid')->group(function () {
+Route::prefix('spid')->middleware(['web'])->group(function () {
     Route::post('sso/auth', [RmsSpidController::class, 'ssoAuth'])->name('spid.sso.auth');
     Route::get('sso/login/{user_spid_id}/{redirect_token}', [RmsSpidController::class, 'ssoLogin'])->name('spid.sso.login');
 });
