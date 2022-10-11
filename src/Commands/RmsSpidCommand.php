3fc75d637e1baf4293aa1c2f4e95fed91e185365
Syafiq Unijaya <syafiqazwan.unijaya@gmail.com>
2022-10-11 16:03:54 +0800

    update cmd

--- src/Commands/RmsSpidCommand.php
+++ src/Commands/RmsSpidCommand.php
@@ -3,16 +3,29 @@
 namespace DeveloperUnijaya\RmsSpid\Commands;
 
 use Illuminate\Console\Command;
-
+use DeveloperUnijaya\RmsSpid\Models\UserSpid;
+use Carbon\Carbon;
 class RmsSpidCommand extends Command
 {
-    public $signature = 'spid:command';
+    public $signature = 'spid:reset-expired-token';
 
-    public $description = 'spid:command';
+    public $description = 'Reset all user expired redirect token';
 
     public function handle(): int
     {
-        $this->comment('All done');
+
+        $now = Carbon::now();
+
+        $userSpids = UserSpid::where('redirect_token_expired_at', '<=', $now)->get();
+
+        foreach($userSpids as $userSpid)
+        {
+            $userSpid->redirect_token = null;
+            $userSpid->redirect_token_expired_at = null;
+            $userSpid->save();
+        }
+
+        $this->comment('completed');
 
         return self::SUCCESS;
     }
